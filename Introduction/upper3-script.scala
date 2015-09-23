0e84e3e9531ccf8d5f4ddbed70e0fb2c546b5a7e
shigemk2 <i.am.shige@gmail.com>
2015-09-23 12:49:00 +0900

    コメント

--- Introduction/upper3-script.scala
+++ Introduction/upper3-script.scala
@@ -1,6 +1,9 @@
 // Javaとは違い、1つのファイルの中にいくつでも好きなだけ公開型を持つことができます
 // $ scalac upper3-script.scala
+// 実行
 // $ scala Upper "A" "First" "Scala" "Program"
+// $ scala -cp . Upper Hello World!
+// cpはクラスパス追加
 object Upper {
   def main(args: Array[String]) = {
     args.map(_.toUpperCase()).foreach(printf("%s ",_))
