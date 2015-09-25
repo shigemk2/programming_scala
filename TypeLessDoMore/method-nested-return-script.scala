4f663bf80fee6511c9c6e5de78aafb55ec110944
shigemk2 <i.am.shige@gmail.com>
2015-09-25 19:46:50 +0900

    upCaseメソッドに戻り値の型Stringを指定するまでコンパイルできない

---
+++ TypeLessDoMore/method-nested-return-script.scala
@@ -0,0 +1,12 @@
+// error: method upCase has return statement; needs result type
+// upCaseメソッドに戻り値の型Stringを指定するまでコンパイルできない
+// とりあえずの対策として、returnをやめるという方法がある
+def upCase(s: String) = {
+  if (s.length == 0)
+    return s
+  else
+    s.toUpperCase()
+}
+
+println( upCase("") )
+println( upCase("Hello") )
