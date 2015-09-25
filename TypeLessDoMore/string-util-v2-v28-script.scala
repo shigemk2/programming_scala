145e61468d8a57fcbe88c82addd204e2ac0d8f84
shigemk2 <i.am.shige@gmail.com>
2015-09-25 18:55:15 +0900

    Scala 2.8からデフォルト引数が使えるようになった

---
+++ TypeLessDoMore/string-util-v2-v28-script.scala
@@ -0,0 +1,8 @@
+object StringUtil {
+  def joiner(strings: List[String], separator: String = " "): String =
+    strings.mkString(separator)
+}
+
+import StringUtil._
+
+println( joiner(List("Programming", "Scala")) )
