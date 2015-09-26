24beabccf26973549d01ca407a543fdb43f7bd8a
shigemk2 <i.am.shige@gmail.com>
2015-09-26 14:45:40 +0900

    文字列リテラル

--- TypeLessDoMore/char-literal-script.scala
+++ TypeLessDoMore/char-literal-script.scala
@@ -3,3 +3,13 @@ val b = '\u0041'
 val c = '\012'
 val d = '\t'
 
+val e = "Programming\nScala"
+// エスケープ文字がエスケープ文字として判定されない
+val f = """Programming\nScala"""
+val g = """He exclaimed, "Scala is great!" """
+// "He exclaimed, "Scala is great!" " こういう書き方をしたらエラーになる
+val h = """First line\n
+Second line\t
+
+Fourth line"""
+
