dfcb04c4308bb1d97a6d98750fc3875e59806cc2
shigemk2 <i.am.shige@gmail.com>
2015-09-26 12:58:24 +0900

    コメント

--- TypeLessDoMore/literal-script.scala
+++ TypeLessDoMore/literal-script.scala
@@ -1,3 +1,6 @@
-val i = 12345678901234567890
-val b1: Byte = 128
+// error: integer number too large
+// val i = 12345678901234567890
+// type mismatch
+// val b1: Byte = 128
 val b2: Byte = 127
+println(b2)
