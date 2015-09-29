a689081dbf53b1da1386f779338634b4e2fd2e84
shigemk2 <i.am.shige@gmail.com>
2015-09-29 17:26:47 +0900

    importは相対的

---
+++ TypeLessDoMore/relative-imports.scala
@@ -0,0 +1,7 @@
+import scala.collection.mutable._
+import collection.immutable._    // 「scala」は、すでにインポートされているため
+import _root_.scala.collection._ // 実際の「ルート」からのフルパス
+
+package scala.actors {
+  import remote._                // 「scala.actors」のスコープ内にいる
+}
