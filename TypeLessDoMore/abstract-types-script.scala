887c5447e3c386de395e55b97380ffe3a3b4ee40
shigemk2 <i.am.shige@gmail.com>
2015-09-29 17:55:34 +0900

    抽象型 type

---
+++ TypeLessDoMore/abstract-types-script.scala
@@ -0,0 +1,28 @@
+import java.io._
+
+abstract class BulkReader {
+  type In
+  val source: In
+  def read: String
+}
+
+class StringBulkReader(val source: String) extends BulkReader {
+  type In = String
+  // フィールドをただ返すだけ
+  def read = source
+}
+
+class FileBulkReader(val source: File) extends BulkReader {
+  type In = File
+  // ファイルの中身を読み込んでいる
+  def read = {
+    val in = new BufferedInputStream(new FileInputStream(source))
+    val numBytes = in.available()
+    val bytes = new Array[Byte](numBytes)
+    in.read(bytes, 0, numBytes)
+    new String(bytes)
+  }
+}
+
+println( new StringBulkReader("Hello Scala!").read )
+println( new FileBulkReader(new File("abstract-types-script.scala")).read )
