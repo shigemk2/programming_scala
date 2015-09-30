9f752dfc89e70db8a45feabe80943ef30681f605
shigemk2 <i.am.shige@gmail.com>
2015-09-30 17:33:55 +0900

    省略

--- Rounding/no-dot-better-script.scala
+++ Rounding/no-dot-better-script.scala
@@ -2,3 +2,5 @@ def isEven(n: Int) = (n % 2) == 0
 
 List(1, 2, 3, 4) filter isEven foreach println
 List(1, 2, 3, 4) foreach println
+List(1, 2, 3, 4) foreach( i => println(i) )
+List(1, 2, 3, 4) foreach( println(_) )
