a6f65926a8b0f993b5fa1e2ad4bb879b144d7a09
shigemk2 <i.am.shige@gmail.com>
2015-10-01 11:08:50 +0900

    優先順位

---
+++ Rounding/priority-script.scala
@@ -0,0 +1,10 @@
+println(2.0 * 4.0 / 3.0 * 5.0)
+println(2.0 * 4.0 / 3.0)
+println(((2.0 * 4.0) / 3.0) * 5.0)
+val list = List('b', 'c', 'd')
+println(list)
+// 先頭に要素を追加
+println('a' :: list)
+// 先頭に要素を追加 + リストの結合
+println('a' :: list ++ List('e', 'f'))
+
