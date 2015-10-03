e8923d4d464e08162a8feb83f12d67f20941669c
shigemk2 <i.am.shige@gmail.com>
2015-10-03 11:17:32 +0900

    if

---
+++ Rounding/if-script.scala
@@ -0,0 +1,8 @@
+// if やその他のほとんどの文が実際には式である
+if (2 + 2 == 5) {
+  println("Hello from 1984.")
+} else if (2 + 2 == 3) {
+  println("Hello from Remedial Math class?")
+} else {
+  println("Hello from a non-Orwellian future.")
+}
