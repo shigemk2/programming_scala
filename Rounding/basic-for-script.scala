e1ce9597edd5a4075d1785cf33cc7db5c5cfc7a1
shigemk2 <i.am.shige@gmail.com>
2015-10-03 21:38:23 +0900

    for

---
+++ Rounding/basic-for-script.scala
@@ -0,0 +1,5 @@
+val dogBreads = List("Doberman", "Yorkshire Terrier", "Dachshund",
+  "Scottish Terrier", "Great Dane", "Portuguese Water Dog")
+
+for (bread <- dogBreads) println(bread)
+dogBreads foreach println
