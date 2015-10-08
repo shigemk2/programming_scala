7a9fc286b0a0a2c46acbd028c1e68968452b6cef
shigemk2 <i.am.shige@gmail.com>
2015-10-09 00:01:17 +0900

    マッチ式の中の変数

---
+++ Rounding/match-variable-script.scala
@@ -0,0 +1,8 @@
+import scala.util.Random
+
+val randomInt = new Random().nextInt(10)
+
+randomInt match {
+  case 7 => println("lucky seven")
+  case otherNumber => println("boo, got boring ol' " + otherNumber)
+}
