ad8ff7535a48fdfc46478d56f37543ca31cf4933
shigemk2 <i.am.shige@gmail.com>
2015-10-13 00:43:20 +0900

    コメント

--- Rounding/match-list-script.scala
+++ Rounding/match-list-script.scala
@@ -3,6 +3,7 @@ val willWork = List(1, 3, 23, 90)
 val willNotWork = List(4, 18, 52)
 val empty = List()
 def processList(l: List[Any]): Unit = l match {
+  // ::を使用してリストから先頭と末尾を抽出
   case head :: tail =>
     printf("elem: %s, ", head)
     processList(tail)
