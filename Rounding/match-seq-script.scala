ecb8a65b930e3f9c60f89c9fc8e28f9ffeea7048
shigemk2 <i.am.shige@gmail.com>
2015-10-13 00:19:26 +0900

    シーケンスに対するマッチ

--- Rounding/match-seq-script.scala
+++ Rounding/match-seq-script.scala
@@ -1,3 +1,4 @@
+// シーケンスに対するマッチ
 val willWork = List(1, 3, 23, 90)
 val willNotWork = List(4, 18, 52)
 val empty = List()
@@ -5,5 +6,7 @@ for (l <- List(willWork, willNotWork, empty)) {
   l match {
     case List(_, 3, _, _) => println("Four elements, with the 2nd being '3'.")
     case List(_*) => println("Any other list with 0 or more elements.")
+    // 一応こういう書き方もいいらしい
+    case _ => println("Any other list with 0 or more elements. ANOTHER WRITING.")
   }
 }
