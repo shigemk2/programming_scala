6345791c617890e0833a2c426c167d8fc43d441a
shigemk2 <i.am.shige@gmail.com>
2015-10-13 00:11:35 +0900

    型に対するマッチ

--- Rounding/match-type-script.scala
+++ Rounding/match-type-script.scala
@@ -1,8 +1,11 @@
 // 型に対するマッチ
 val sundries = List(23, "Hello", 8.5, 'q')
 
-for (sundry -> sundries) {
+for (sundry <- sundries) {
   sundry match {
-    case i: Int -> println("got an Integer: " + i)
+    case i: Int => println("got an Integer: " + i)
+    case s: String => println("got a String: " + s)
+    case f: Double => println("got a Double: " + f)
+    case other => println("got something else: " + other)
   }
 }
