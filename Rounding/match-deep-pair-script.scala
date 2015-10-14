430062f75f39bf30bc45a414904248d819141242
shigemk2 <i.am.shige@gmail.com>
2015-10-15 00:59:58 +0900

    p @ Person(...)構文を使うと、入れ子のマッチ文を1つの文に平坦化できます

--- Rounding/match-deep-pair-script.scala
+++ Rounding/match-deep-pair-script.scala
@@ -10,6 +10,7 @@ val bob = new Person("Bob", 32, Manager)
 val charlie = new Person("Charlie", 32, Developer)
 for (item <- Map(1 -> alice, 2 -> bob, 3 -> charlie)) {
   item match {
+    // p @ Person(...)構文を使うと、入れ子のマッチ文を1つの文に平坦化できます
     case (id, p @ Person(_, _, Manager)) => printf("%s is overpaid.\n", p)
     case (id, p @ Person(_, _, _)) => printf("%s is underpaid.\n", p)
   }
