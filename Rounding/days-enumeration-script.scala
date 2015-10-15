17f753fe941cbfc4d1b1a7226a4d009bac551e7e
shigemk2 <i.am.shige@gmail.com>
2015-10-16 00:11:47 +0900

    enumeration(やっぱり動かない)

---
+++ Rounding/days-enumeration-script.scala
@@ -0,0 +1,7 @@
+object WeekDay extends Enumeration {
+  type WeekDay = Value
+  val Mon, Tue, Wed, Thu, Fri, Sat, Sun = Value
+}
+import WeekDay._
+def isWorkingDay(d: WeekDay) = ! (d == Sat || d == Sun)
+WeekDay filter isWorkingDay foreach println
