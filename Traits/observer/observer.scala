5aee1730825f927585522c0afe423c69c550c8cc
shigemk2 <i.am.shige@gmail.com>
2015-10-19 23:11:29 +0900

    コメント

--- Traits/observer/observer.scala
+++ Traits/observer/observer.scala
@@ -3,5 +3,7 @@ trait Subject {
   type Observer = { def receiveUpdate(subject: Any) }
   private var observers = List[Observer]()
   def addObserver(observer:Observer) = observers ::= observer
+  // こういうふうに書くこともできる
+  // def addObserver(observer:Observer) = observers = observer :: observers
   def notifyObservers = observers foreach (_.receiveUpdate(this))
 }
