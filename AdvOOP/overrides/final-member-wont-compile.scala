f6ba560704a84048b0cfd760b285af779bce5277
shigemk2 <i.am.shige@gmail.com>
2015-11-18 01:00:22 +0900

    final comment

--- AdvOOP/overrides/final-member-wont-compile.scala
+++ AdvOOP/overrides/final-member-wont-compile.scala
@@ -2,5 +2,6 @@ class NotFixed {
   final def fixedMethod = "fixed"
 }
 class Changeable2 extends NotFixed {
+  // method fixedMethod cannot override final member
   override def fixedMethod = "not fixed" // エラー
 }
