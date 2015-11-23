a832e41491090ad05a33f52282273bd1a4ee44a0
shigemk2 <i.am.shige@gmail.com>
2015-11-23 18:29:43 +0900

    修正 withを使うといい感じにやってくれる

--- AdvOOP/overrides/trait-invalid-init-val-script.scala
+++ AdvOOP/overrides/trait-invalid-init-val-script.scala
@@ -1,12 +1,13 @@
-// エラー：初期化前に「value」が読み取られる。
+// with AbstractT2節の前のブロックの中で、フィールドvalueを初期化
 trait AbstractT2 {
   println("In AbstractT2:")
   val value: Int
-  val inverse = 1.0/value // ???
+  val inverse = 1.0/value
   println("AbstractT2: value = "+value+", inverse = "+inverse)
 }
-val c2b = new AbstractT2 {
-  println("In c2b:")
+val c2c = new {
+  // 事前初期化ブロックの中では初期化のみが可能
+  // println("In c2c:")
   val value = 10
-}
-println("c2b.value = "+c2b.value+", inverse = "+c2b.inverse)
+} with AbstractT2
+println("c2c.value = "+c2c.value+", inverse = "+c2c.inverse)
