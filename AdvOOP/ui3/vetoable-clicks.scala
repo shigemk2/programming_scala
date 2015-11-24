48bbdad0c02d2c847f908e302b1b84637784bec6
shigemk2 <i.am.shige@gmail.com>
2015-11-25 00:51:12 +0900

    vetoable clicks

---
+++ AdvOOP/ui3/vetoable-clicks.scala
@@ -0,0 +1,11 @@
+package ui3
+import observer._
+trait VetoableClicks extends Clickable {
+  var maxAllowed = 1 // default
+  private var count = 0
+  abstract override def click() = {
+    count += 1
+    if (count <= maxAllowed)
+      super.click()
+  }
+}
