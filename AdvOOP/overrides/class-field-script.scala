860fe430951ef0b574abdafd7fd84b62e9535446
shigemk2 <i.am.shige@gmail.com>
2015-11-26 00:34:35 +0900

    c1

--- AdvOOP/overrides/class-field-script.scala
+++ AdvOOP/overrides/class-field-script.scala
@@ -6,6 +6,10 @@ class ClassWithC1 extends C1 {
   override val name = "ClassWithC1"
   count = 1
 }
+val c1 = new C1()
+println(c1.name)
+println(c1.count)
+
 val c = new ClassWithC1()
 println(c.name)
 println(c.count)
