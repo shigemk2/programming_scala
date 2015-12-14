550467fdc557de82efe08d079d50d5677ab80d99
shigemk2 <i.am.shige@gmail.com>
2015-12-15 00:47:09 +0900

    error

--- AdvOOP/overrides/method-field-trait-script.scala
+++ AdvOOP/overrides/method-field-trait-script.scala
@@ -3,6 +3,7 @@ trait NameTrait {
   def name = "NameTrait"
 }
 class ConcreteNameClass extends NameTrait {
+  // val name = "ConcreteNameClass" // error
   override val name = "ConcreteNameClass"
 }
 println(new ConcreteNameClass().name) // => "ConcreteNameClass"
