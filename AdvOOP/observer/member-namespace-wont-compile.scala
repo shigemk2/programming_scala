0a29e14c0a2fac14364904cbe3bf2630c94e9b3b
shigemk2 <i.am.shige@gmail.com>
2015-12-22 00:32:18 +0900

    コメント

--- AdvOOP/observer/member-namespace-wont-compile.scala
+++ AdvOOP/observer/member-namespace-wont-compile.scala
@@ -2,7 +2,7 @@
 class IllegalMemberNameUse {
   def member(i: Int) = 2 * i
   val member = 2 // エラー
-  object member { // エラー
+  object member { // エラー // member is already defined as value member
     def apply() = 2
   }
 }
