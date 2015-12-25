850bb0ca9dfdea5967311bdddb1d21384f5e4f60
shigemk2 <i.am.shige@gmail.com>
2015-12-26 04:42:58 +0900

    シンボル

--- AdvOOP/objects/list-apply-example-script.scala
+++ AdvOOP/objects/list-apply-example-script.scala
@@ -1,6 +1,6 @@
 // 暗黙のうちにList.applyが使われるスクリプトの例を
 val list1 = List()
-val list2 = List(1, 2.2, "three", 'four)
+val list2 = List(1, 2.2, "three", 'four) // 'fourはシンボル
 val list3 = List.apply("1", "2.2", "three", "four") // こういうやつ
 println("1: "+list1)
 println("2: "+list2)
