1946e0acadbbbd380c885c23b60ccc98c45ede2b
shigemk2 <i.am.shige@gmail.com>
2015-12-30 01:16:21 +0900

    ::という名前のケースクラスのコンパニオンオブジェクト

--- Rounding/match-list-script.scala
+++ Rounding/match-list-script.scala
@@ -4,7 +4,9 @@ val willNotWork = List(4, 18, 52)
 val empty = List()
 def processList(l: List[Any]): Unit = l match {
   // ::を使用してリストから先頭と末尾を抽出
-  case head :: tail =>
+  // case head :: tail =>
+  // こいつは↑と等価
+  case ::(head, tail) =>
     printf("elem: %s, ", head)
     processList(tail)
   case Nil => println("")
