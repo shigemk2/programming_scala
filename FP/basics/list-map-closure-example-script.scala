71f7d196085ac7cc34db125e232d1dc73985c0f4
shigemk2 <i.am.shige@gmail.com>
2016-01-03 21:58:56 +0900

    ラムダ

---
+++ FP/basics/list-map-closure-example-script.scala
@@ -0,0 +1,7 @@
+var factor = 3
+val multiplier = (i:Int) => i * factor
+val l1 = List(1, 2, 3, 4, 5) map multiplier
+factor = 5
+val l2 = List(1, 2, 3, 4, 5) map multiplier
+println(l1)
+println(l2)
