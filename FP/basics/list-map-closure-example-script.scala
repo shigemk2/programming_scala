0a7d588e5c93a317109a0028dfaedc794abc511a
shigemk2 <i.am.shige@gmail.com>
2016-01-04 22:08:40 +0900

    コメント

--- FP/basics/list-map-closure-example-script.scala
+++ FP/basics/list-map-closure-example-script.scala
@@ -1,3 +1,5 @@
+// multiplierは不変な関数値であるにもかかわらず、factorが変更されるとその振る舞いが変わりました。
+// multiplierには2つの自由変数であるiとfactorがあります。そのうちの1つiは関数の仮引数です。したがって、multiplierが呼び出されるたびに、iは新しい値に束縛されます
 var factor = 3
 val multiplier = (i:Int) => i * factor
 val l1 = List(1, 2, 3, 4, 5) map multiplier
