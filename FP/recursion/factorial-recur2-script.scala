7a3774a158f3a2cea92a31ca4fc05eae005ff896
shigemk2 <i.am.shige@gmail.com>
2016-01-06 00:48:49 +0900

    コメント

--- FP/recursion/factorial-recur2-script.scala
+++ FP/recursion/factorial-recur2-script.scala
@@ -1,3 +1,5 @@
+// ループによってスタックオーバーフローの可能性がなくなり、再帰的な関数呼び出しのオーバーヘッドを取り除くことによってパフォーマンスが向上します
+// 末尾再帰の最適化はまだJVMによってサポートされていませんが、scalacは最適化を行うことができます
 def factorial(i: BigInt): BigInt = {
   def fact(i: BigInt, accumulator: BigInt): BigInt = i match {
     case _ if i == 1 => accumulator
