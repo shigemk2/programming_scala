7ba5e843b332db873e7f4b31b63c23e1a77f05e0
shigemk2 <i.am.shige@gmail.com>
2016-01-13 00:16:27 +0900

    map

---
+++ FP/datastructs/map-script.scala
@@ -0,0 +1,6 @@
+val stateCapitals = Map(
+  "Alabama" -> "Montgomery",
+  "Alaska" -> "Juneau",
+  "Wyoming" -> "Cheyenne")
+val lengths = stateCapitals map { kv => (kv._1, kv._2.length) }
+println(lengths)
