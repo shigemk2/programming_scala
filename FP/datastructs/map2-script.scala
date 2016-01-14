a13f434a27389038d716d7189d0560d3158be7b6
shigemk2 <i.am.shige@gmail.com>
2016-01-15 00:35:00 +0100

    map

---
+++ FP/datastructs/map2-script.scala
@@ -0,0 +1,8 @@
+val stateCapitals = Map(
+  "Alabama" -> "Montgomery",
+  "Alaska" -> "Juneau",
+  "Wyoming" -> "Cheyenne")
+val map2 = stateCapitals map { kv => (kv._1, kv._2.length) }
+// val lengths = Map(map2) // エラー：うまくいかない
+val lengths = Map[String,Int]() ++ map2
+println(lengths)
